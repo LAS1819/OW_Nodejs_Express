ac0d72f654b6f98d43131520405ae01e08312717
Lluís Aracil Sabater <aracil17652@iesmarenostrum.com>
2020-05-14 16:55:02 +0200

    Finalizado ejercicio de la Home

--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
 // Importamos módulo HTTP de node
 import http from 'http'
+// Importamos la librería file system para leer el html en este caso
+import fs from 'fs'
+
+// Guardamos en una constante el fichero que vamos a leer
+const file = './src/index.html'
 
 // Creamos servidor
 const server = http.createServer((request, response) => {
-	
+	// Advertimos que la cabecera correcta (200) es HTML y está codificada en UTF-8
+	response.writeHead(200, {'Content-Type': 'text/html; charset:UTF-8'})
+	//response.writeHead(200, {'Content-Type': 'application/json'})
+
+	// Leemos el fichero con fs
+	fs.readFile(file, (err, content) => {
+		if (err) {
+			return cosnole.log(err)
+		}
+
+		// Si no hay error escribimos el fichero
+		response.write(content)
+		// Finalizamos respuesta
+		response.end()
+	})
+
 })
+
+
 
 // Asignamos el puerto 8000 al servidor localhost
 server.listen(8000, 'localhost', err => {
