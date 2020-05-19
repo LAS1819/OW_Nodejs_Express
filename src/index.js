f8901565e2e9c359e79af0e67e1a0e1f6060be0d
Lluís Aracil Sabater <aracil17652@iesmarenostrum.com>
2020-05-19 22:55:38 +0200

    Conexión con servidor actualizado

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
-import express from 'express'
+import express from 'express';
+import morgan from 'morgan';
 
-const app = express()
+const app = express();
+// Desactivamos el aviso de que estamos usando Express en nuestra cabecera
+app.disable('x-powered-by');
+
+// Le decimos al servidor que estamos en desarrollo
+app.set('env', 'development');
+
+// Agregamos un loger con morgan
+// combined = Hace que se muestre otro tipo de log
+app.use(morgan('combined'));
 
 app.get('/', (req, res) => {
 	res.end('Hola Mundo!')
