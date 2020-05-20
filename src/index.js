d3d0f0aa3924a888a99d2b4b39f15d94b54b47e4
Lluís Aracil Sabater <aracil17652@iesmarenostrum.com>
2020-05-21 00:05:31 +0200

    Launching tests

--- src/index.js
+++ src/index.js
@@ -6,57 +6,78 @@ import path from 'path';
 // Importamos el router que hemos creado
 import router from './router'
 
-const app = express();
+let _server
 
-// Desactivamos el aviso de que estamos usando Express en nuestra cabecera
-app.disable('x-powered-by');
+const server={
+	start () {
+		const app = express();
 
-
-
-// Le decimos al servidor que estamos en desarrollo
-app.set('env', 'development');
-
-// Agregamos un loger con morgan
-// combined = Hace que se muestre otro tipo de log
-app.use(morgan('tiny'));
-
-// Queremos que use Json
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+		// Desactivamos el aviso de que estamos usando Express en nuestra cabecera
+		app.disable('x-powered-by');
 
 
 
+		// Le decimos al servidor que estamos en desarrollo
+		//app.set('env', 'development');
 
-// Decimos que vamos a utilizar como motor de plantilla PUG y nuestras vistas están en un directorio en concreto
-// src/views
-app.set('views', path.join(__dirname, 'views'));
+		// Definimos como entorno de test
+		app.set('env', process.env.NODE_ENV);
 
-// Decimos el motor de plantilla
-app.set('view engine', 'pug');
+		if (process.env.NODE_ENV !== 'tests') {
+			// Agregamos un logger con morgan
+			// combined = Hace que se muestre otro tipo de log
+			app.use(morgan('tiny'));
+		}
 
-
-// Rutas
-router(app)
-
-
-// app.get('/static', (req, res) => {
-// 	path.join(__dirname, `public/${req.url}`)
-// })
-// La anterior ruta se puede hacer más fácilmente con
-app.use('/static', express.static(path.join(__dirname, 'public')))
-
-// ------------Midleware----------
-// Si usa una ruta que no está definida, le enviamos un error 404
-app.use((req, res, next) => {
-	res.status(404).render('404', {
-		title: 'Openwebinars - Error',
-		message: 'La ruta no existe.'
-	})
-	next(err)
-	res.end()
-})
+		// Queremos que use Json
+		app.use(bodyParser.json());
+		app.use(bodyParser.urlencoded({extended: false}));
 
 
-app.listen('9000', () => {
-	console.log('Servidor arrancado en http://localhost:9000')
-});
+
+
+		// Decimos que vamos a utilizar como motor de plantilla PUG y nuestras vistas están en un directorio en concreto
+		// src/views
+		app.set('views', path.join(__dirname, 'views'));
+
+		// Decimos el motor de plantilla
+		app.set('view engine', 'pug');
+
+
+		// Rutas
+		router(app)
+
+
+		// app.get('/static', (req, res) => {
+		// 	path.join(__dirname, `public/${req.url}`)
+		// })
+		// La anterior ruta se puede hacer más fácilmente con
+		app.use('/static', express.static(path.join(__dirname, 'public')))
+
+		// ------------Midleware----------
+		// Si usa una ruta que no está definida, le enviamos un error 404
+		app.use((req, res, next) => {
+			res.status(404).render('404', {
+				title: 'Openwebinars - Error',
+				message: 'La ruta no existe.'
+			})
+			next(err)
+			res.end()
+		});
+
+
+		_server = app.listen('9000', () => {
+			console.log('Servidor arrancado en http://localhost:9000')
+		});
+	},
+	close () {
+		_server.close()
+	}
+};
+
+export default server
+
+if (!module.parent) {
+	server.start()
+};
+
