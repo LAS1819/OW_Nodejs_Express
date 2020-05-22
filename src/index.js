9e78d2c3bc4f2212d02a7e81186bf00e220fe675
Lluís Aracil Sabater <aracil17652@iesmarenostrum.com>
2020-05-22 14:23:53 +0200

    Routing

--- src/index.js
+++ src/index.js
@@ -12,6 +12,31 @@ const server = {
 		// Añadimos la configuración a la App
 		config(app);
 
+		// Rutas
+		app.get('/', (req, res, next) => {
+			res
+			.status(200)
+			.json({data: 'Método get'})
+		});
+
+		app.post('/resource', (req, res, next) => {
+			res
+			.status(201)
+			.json({data: 'Método post'})
+		})
+
+		app.put('/', (req, res, next) => {
+			res
+			.status(201)
+			.json({ data: 'Método put'})
+		})
+
+		app.delete('/', (req, res, next) => {
+			res
+			.status(200)
+			.json({ data: 'Método delete' })
+		})
+
 		_server = app.listen('9000', () => {
 			if (process.env.NODE_ENV !== 'test') {
 				console.log('Servidor abierto en http://localhost:9000')
